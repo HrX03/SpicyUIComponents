7044b8c54dc3c18da73b158a1ec6a6d3d62b4ed5
HrX03 <dn.bianco03@gmail.com>
2020-05-10 13:46:33 +0200

    components: Update themes for flutter 1.17

--- lib/src/resources/themes.dart
+++ lib/src/resources/themes.dart
@@ -1,94 +1,158 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 
 class SpicyThemes {
   static ThemeData black(Color dynamicAccentColor) {
     return ThemeData.dark().copyWith(
-        accentColor: dynamicAccentColor,
+      accentColor: dynamicAccentColor,
+      backgroundColor: dynamicAccentColor,
+      bottomAppBarColor: Colors.black,
+      canvasColor: Colors.black,
+      cardColor: Color(0xFF121212),
+      cursorColor: dynamicAccentColor,
+      dialogBackgroundColor: Colors.black,
+      primaryColor: dynamicAccentColor,
+      primaryColorLight: Colors.black,
+      secondaryHeaderColor: Colors.black,
+      scaffoldBackgroundColor: Colors.black,
+      textSelectionHandleColor: dynamicAccentColor,
+      snackBarTheme: SnackBarThemeData(
+        backgroundColor: Color(0xFF121212),
+        elevation: 1,
+        actionTextColor: dynamicAccentColor,
+        contentTextStyle: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      colorScheme: ColorScheme.dark(
+        surface: Colors.black,
+        primary: dynamicAccentColor,
+        onPrimary: Color(0xFF121212),
+      ),
+      textSelectionColor: dynamicAccentColor,
+      buttonTheme: ButtonThemeData(
+        textTheme: ButtonTextTheme.accent,
+        hoverColor: dynamicAccentColor,
+      ),
+      bottomSheetTheme: BottomSheetThemeData(
+        modalBackgroundColor: Color(0xFF121212),
+        shape: RoundedRectangleBorder(),
+      ),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
         backgroundColor: dynamicAccentColor,
-        bottomAppBarColor: Colors.black,
-        canvasColor: Colors.black,
-        cardColor: Color(0xFF121212),
-        cursorColor: dynamicAccentColor,
-        dialogBackgroundColor: Colors.black,
-        primaryColor: dynamicAccentColor,
-        primaryColorLight: Colors.black,
-        secondaryHeaderColor: Colors.black,
-        scaffoldBackgroundColor: Colors.black,
-        textSelectionHandleColor: dynamicAccentColor,
-        snackBarTheme: SnackBarThemeData(
-          backgroundColor: Color(0xFF121212),
-          elevation: 1,
-          actionTextColor: dynamicAccentColor,
-          contentTextStyle: TextStyle(
-            color: Colors.white,
+        foregroundColor: Color(0xFF121212),
+      ),
+      buttonColor: dynamicAccentColor,
+      iconTheme: IconThemeData(color: Colors.white.withOpacity(0.7)),
+      disabledColor: Colors.white.withOpacity(0.4),
+      pageTransitionsTheme: const PageTransitionsTheme(
+        builders: <TargetPlatform, PageTransitionsBuilder>{
+          TargetPlatform.android: SharedAxisPageTransitionsBuilder(
+            transitionType: SharedAxisTransitionType.scaled,
           ),
-        ),
-        textSelectionColor: dynamicAccentColor,
-        buttonTheme: ButtonThemeData(
-          textTheme: ButtonTextTheme.accent,
-          hoverColor: dynamicAccentColor,
-        ),
-        buttonColor: dynamicAccentColor,
-        iconTheme: IconThemeData(color: Colors.white.withOpacity(0.7)),
-        disabledColor: Colors.white.withOpacity(0.4));
+        },
+      ),
+    );
   }
 
   static ThemeData dark(Color dynamicAccentColor) {
     return ThemeData.dark().copyWith(
-        accentColor: dynamicAccentColor,
-        cursorColor: dynamicAccentColor,
-        textSelectionHandleColor: dynamicAccentColor,
-        snackBarTheme: SnackBarThemeData(
-          backgroundColor: Colors.grey[900],
-          elevation: 1,
-          actionTextColor: dynamicAccentColor,
-          contentTextStyle: TextStyle(
-            color: Colors.white,
-          ),
+      accentColor: dynamicAccentColor,
+      cursorColor: dynamicAccentColor,
+      textSelectionHandleColor: dynamicAccentColor,
+      snackBarTheme: SnackBarThemeData(
+        backgroundColor: Colors.grey[900],
+        elevation: 1,
+        actionTextColor: dynamicAccentColor,
+        contentTextStyle: TextStyle(
+          color: Colors.white,
         ),
-        textSelectionColor: dynamicAccentColor,
-        buttonTheme: ButtonThemeData(
-          textTheme: ButtonTextTheme.accent,
-          hoverColor: dynamicAccentColor,
-        ),
-        buttonColor: dynamicAccentColor,
-        primaryColor: dynamicAccentColor,
+      ),
+      colorScheme: ColorScheme.dark(
+        surface: Color(0xFF151618),
+        primary: dynamicAccentColor,
+        onPrimary: Colors.grey[900],
+      ),
+      textSelectionColor: dynamicAccentColor,
+      buttonTheme: ButtonThemeData(
+        textTheme: ButtonTextTheme.accent,
+        hoverColor: dynamicAccentColor,
+      ),
+      bottomSheetTheme: BottomSheetThemeData(
+        modalBackgroundColor: Colors.grey[900],
+        shape: RoundedRectangleBorder(),
+      ),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
         backgroundColor: dynamicAccentColor,
-        cardColor: Colors.grey[900],
-        scaffoldBackgroundColor: Color(0xFF151618),
-        dialogBackgroundColor: Colors.grey[900],
-        bottomAppBarColor: Colors.grey[900],
-        canvasColor: Colors.grey[900],
-        iconTheme: IconThemeData(color: Colors.white.withOpacity(0.7)),
-        disabledColor: Colors.white.withOpacity(0.4));
+        foregroundColor: Colors.grey[900],
+      ),
+      buttonColor: dynamicAccentColor,
+      primaryColor: dynamicAccentColor,
+      backgroundColor: dynamicAccentColor,
+      cardColor: Colors.grey[900],
+      scaffoldBackgroundColor: Color(0xFF151618),
+      dialogBackgroundColor: Colors.grey[900],
+      bottomAppBarColor: Colors.grey[900],
+      canvasColor: Colors.grey[900],
+      iconTheme: IconThemeData(color: Colors.white.withOpacity(0.7)),
+      disabledColor: Colors.white.withOpacity(0.4),
+      pageTransitionsTheme: const PageTransitionsTheme(
+        builders: <TargetPlatform, PageTransitionsBuilder>{
+          TargetPlatform.android: SharedAxisPageTransitionsBuilder(
+            transitionType: SharedAxisTransitionType.scaled,
+          ),
+        },
+      ),
+    );
   }
 
   static ThemeData light(Color dynamicAccentColor) {
     Color scaffoldBackup = ThemeData.light().scaffoldBackgroundColor;
 
     return ThemeData.light().copyWith(
-        accentColor: dynamicAccentColor,
-        cursorColor: dynamicAccentColor,
-        textSelectionHandleColor: dynamicAccentColor,
-        snackBarTheme: SnackBarThemeData(
-          backgroundColor: ThemeData.light().cardColor,
-          elevation: 1,
-          actionTextColor: dynamicAccentColor,
-          contentTextStyle: TextStyle(
-            color: Colors.black,
-          ),
+      accentColor: dynamicAccentColor,
+      cursorColor: dynamicAccentColor,
+      textSelectionHandleColor: dynamicAccentColor,
+      snackBarTheme: SnackBarThemeData(
+        backgroundColor: ThemeData.light().cardColor,
+        elevation: 1,
+        actionTextColor: dynamicAccentColor,
+        contentTextStyle: TextStyle(
+          color: Colors.black,
         ),
-        textSelectionColor: dynamicAccentColor,
-        buttonTheme: ButtonThemeData(
-          textTheme: ButtonTextTheme.accent,
-          hoverColor: dynamicAccentColor,
-        ),
-        scaffoldBackgroundColor: scaffoldBackup,
-        cardColor: ThemeData.light().cardColor,
-        buttonColor: dynamicAccentColor,
-        primaryColor: dynamicAccentColor,
+      ),
+      colorScheme: ColorScheme.light(
+        surface: scaffoldBackup,
+        primary: dynamicAccentColor,
+        onPrimary: ThemeData.light().cardColor,
+      ),
+      textSelectionColor: dynamicAccentColor,
+      buttonTheme: ButtonThemeData(
+        textTheme: ButtonTextTheme.accent,
+        hoverColor: dynamicAccentColor,
+      ),
+      bottomSheetTheme: BottomSheetThemeData(
+        modalBackgroundColor: ThemeData.light().cardColor,
+        shape: RoundedRectangleBorder(),
+      ),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
         backgroundColor: dynamicAccentColor,
-        iconTheme: IconThemeData(color: Colors.black.withOpacity(0.7)),
-        disabledColor: Colors.black.withOpacity(0.7));
+        foregroundColor: ThemeData.light().cardColor,
+      ),
+      scaffoldBackgroundColor: scaffoldBackup,
+      cardColor: ThemeData.light().cardColor,
+      buttonColor: dynamicAccentColor,
+      primaryColor: dynamicAccentColor,
+      backgroundColor: dynamicAccentColor,
+      iconTheme: IconThemeData(color: Colors.black.withOpacity(0.7)),
+      disabledColor: Colors.black.withOpacity(0.7),
+      pageTransitionsTheme: const PageTransitionsTheme(
+        builders: <TargetPlatform, PageTransitionsBuilder>{
+          TargetPlatform.android: SharedAxisPageTransitionsBuilder(
+            transitionType: SharedAxisTransitionType.scaled,
+          ),
+        },
+      ),
+    );
   }
 }
